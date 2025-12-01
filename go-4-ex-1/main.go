ae155a68ce375dd509f3c8acc045c16b4d6e3d3a
leo <leonardoilprofessore@gmail.com>
2025-12-01 14:15:36 +0100

    task 1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,16 @@
 package main
+import "fmt"
 
 // TODO: implement the function computeGrade
+func computeGrade(score float32, maxpoints float32) float32 {
+	grade := (score / maxpoints) * 5 + 1
+	return grade
+}
+
+
 
 func main() {
 	// TODO: call the function computeGrade
+	grade := computeGrade(85, 100)
+	fmt.Printf("Grade: %.2f\n", grade)
 }
